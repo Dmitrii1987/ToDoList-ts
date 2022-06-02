6ae68bf5cb1bc2dedc82423d3f98b778ab21d70f
Dmitrii1987 <62569977+Dmitrii1987@users.noreply.github.com>
2022-06-02 16:44:21 +0300

    feat: do destructurisation, add useState,setTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ToDoList, { TaskType } from './ToDoList';
 
 function App() {
-    //BLL(business logic layer)
+    //BLL(business logic layer):
+    // тут выполнена деструктуризация(почитать про это)
+    const [tasks, setTasks] = useState<Array<TaskType>>([
+                        {id: 1, title: "Sleep", isDone: true},
+                        {id: 2, title: "Run", isDone: true},
+                        {id: 3, title: "Eat", isDone: false},
+                            ]);
 
-    const tasks: Array<TaskType> = [
-        {id: 1, title: "Sleep", isDone: true},
-        {id: 2, title: "Run", isDone: true},
-        {id: 3, title: "Eat", isDone: false},
-    ]
 
     const removeTask = (taskID: number) => {
-        tasks.filter(t => t.id !== taskID)
-        console.log(tasks)
+        const filteredTasks = tasks.filter(t => t.id !== taskID)
+        setTasks(filteredTasks);
     }
     
     //UI(user interface):
     return (
-        <div className="App"> {/* ToDoList({title: "What to do"}) это то, что под капотом*/}
+        <div className="App"> 
             <ToDoList
             title={"What to do"}
             tasks={tasks}
